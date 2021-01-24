dda6a5ba13c4d679db175dddb7e0bf15fa6af409
LT-6FGD373\Hannes <h.roever@zertificon.com>
2021-01-24 13:00:38 +0100

    added try catch in main

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from UserSelectionView import UserSelectionView
 
-UserSelectionView.initMainWindow()
+try: 
+    UserSelectionView.initMainWindow()
+except: 
+    print('Das Programm konnte nicht geladen werden, wahrscheinlich fehlen Abhängigkeiten.\n' +
+    'Diese sollten eigentlich über dieses programm installiert werden, das hat nicht geklappt :-(\n'+
+    'Folgende Bibliotheken müssen manuell installiert werden (mit Befehl zur Installation via PowerShell:\n'+
+    'PySimpleGUI        <pip install PySimpleGUI>\n'+
+    'Plotly Express     <pip install plotly-express>\n'+
+    'Kaleido            <pip install kaleido>\n'+
+    'Pandas             <pip install pandas>'
+    )
+
